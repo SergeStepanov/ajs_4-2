927d1e7b00d1094ece2db16f39fe092692d0f1e8
Sergey Stepanov <ser1653285@yandex.ru>
2021-10-24 14:00:05 +0300

    fix test.js

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,17 +1,21 @@
 import sortLive from '../index';
 
+const arrSort = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+const arrTotal = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
 test('тест функции sortLive', () => {
-  const arrSort = [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ];
-
-  const arrTotal = [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ];
-
   expect(sortLive(arrSort)).toEqual(arrTotal);
+});
+
+test('тест с  .toBe', () => {
+  expect(sortLive(arrSort)).toBe(arrTotal);
 });
